dd3fe39f9fb8a82c37b7e96efd21c43ab4206429
violekdev <71582866+violekdev@users.noreply.github.com>
2023-10-22 18:38:30 +0530

    Updated Image UI

--- lib/pokedex/widgets/pokedex_widget.dart
+++ lib/pokedex/widgets/pokedex_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -44,26 +45,10 @@ class PokedexWidget extends StatelessWidget {
                 BlocProvider.of<PokemonBloc>(context).add(GetPokemonList(url: pokedex.pokemonSearchResults[index].pokemonIndexUrl)),
                 context.goNamed(AppRouter.pokemonDetails),
               },
-              // icon: ModelViewer(
-              //   backgroundColor: Colors.white,
-              //   src: "https://github.com/PoGo-Devs/PoGo-3D-Assets/blob/master/Pokemon-Gen1/001_Bulbasaur/Bulbasaur.fbx",
-              //   // src: "assets/Bulbasaur.c4d",
-              //   alt: "A 3D model of an table soccer",
-              //   autoPlay: true,
-              //   autoRotate: true,
-              //   key: Key(pokedex.pokemonSearchResults![index].name!),
-              // ),
-              // icon: Image.network(
-              //     "https://github.com/PoGo-Devs/PoGo-3D-Assets/blob/master/Pokemon-Gen1/001_Bulbasaur/001_Bulbasaur_Anim.gif"),
-              // icon: CachedNetworkImage(
-              //   imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png",
-              //   // progressIndicatorBuilder: (context, url, downloadProgress) =>
-              //   //     CircularProgressIndicator(value: downloadProgress.progress),
-              //   // errorWidget: (context, url, error) => Icon(Icons.error),
-              // ),
-              icon: Image.network('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png'),
-
-              // const Icon(Icons.workspace_premium_outlined),
+              icon: CachedNetworkImage(
+                imageUrl: pokedex.pokemonSearchResults[index].pokemonModelUrl,
+                cacheKey: pokedex.pokemonSearchResults[index].pokemonModelUrl,
+              ),
             ),
           ),
         );
