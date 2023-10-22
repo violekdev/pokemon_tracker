56d34b669fffdcf6fb829d5bf72bdde7ac00d293
violekdev <71582866+violekdev@users.noreply.github.com>
2023-10-23 00:28:04 +0530

    Updated Router and Home

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pokemon_tracker/app/core/core.dart';
-import 'package:pokemon_tracker/home/home.dart';
 import 'package:pokemon_tracker/pokedex/view/pokedex.dart';
 import 'package:pokemon_tracker/pokemon/view/pokemon.dart';
 import 'package:pokemon_tracker/splash_screen/splash_screen.dart';
@@ -12,44 +11,35 @@ class AppRouter {
   final GoRouter goRouter; // This instance will be store route state
 
   static const String home = '/';
-  static const String splash = 'splash';
-  static const String pokedex = 'pokedex';
+  static const String splash = '/splash';
+  static const String pokedex = '/pokedex';
   static const String pokemonDetails = 'pokemonDetails';
 
   static GoRouter get _router => GoRouter(
-        // initialLocation: splash,
+        initialLocation: pokedex,
         routes: [
           GoRoute(
-            name: home,
-            path: home,
+            name: pokedex,
+            path: pokedex,
             builder: (BuildContext context, GoRouterState state) {
-              return const HomeScreen(key: Key(Keys.homeScreen));
+              return const PokedexScreen(key: Key(Keys.pokedexScreen));
             },
             routes: [
               GoRoute(
-                name: splash,
-                path: splash,
+                name: pokemonDetails,
+                path: pokemonDetails,
                 builder: (BuildContext context, GoRouterState state) {
-                  return const SplashScreen(key: Key(Keys.splashScreen));
+                  return const PokemonDetailsScreen(key: Key(Keys.pokemonDetailsScreen));
                 },
-              ),
-              GoRoute(
-                name: pokedex,
-                path: pokedex,
-                builder: (BuildContext context, GoRouterState state) {
-                  return const PokedexScreen(key: Key(Keys.pokedexScreen));
-                },
-                routes: [
-                  GoRoute(
-                    name: pokemonDetails,
-                    path: pokemonDetails,
-                    builder: (BuildContext context, GoRouterState state) {
-                      return const PokemonDetailsScreen(key: Key(Keys.pokemonDetailsScreen));
-                    },
-                  ),
-                ],
               ),
             ],
+          ),
+          GoRoute(
+            name: splash,
+            path: splash,
+            builder: (BuildContext context, GoRouterState state) {
+              return const SplashScreen(key: Key(Keys.splashScreen));
+            },
           ),
         ],
         errorBuilder: (context, state) => Container(),
--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:pokemon_tracker/app/router/app_router.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:pokemon_tracker/app/router/app_router.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ColoredBox(
-          color: Colors.lightBlueAccent,
-          child: ListView(
-            children: [
-              ListTile(
-                title: TextButton(
-                  onPressed: () => context.goNamed(AppRouter.pokedex),
-                  child: const Text('Pokedex'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: ColoredBox(
+//           color: Colors.lightBlueAccent,
+//           child: ListView(
+//             children: [
+//               ListTile(
+//                 title: TextButton(
+//                   onPressed: () => context.goNamed(AppRouter.pokedex),
+//                   child: const Text('Pokedex'),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
