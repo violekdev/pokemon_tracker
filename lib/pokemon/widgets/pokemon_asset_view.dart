d32732ee33948712964ea38f15b2a1f39c7b9c3b
violekdev <71582866+violekdev@users.noreply.github.com>
2024-03-09 10:31:36 +0530

    UI Changes

--- lib/pokemon/widgets/pokemon_asset_view.dart
+++ lib/pokemon/widgets/pokemon_asset_view.dart
@@ -14,30 +14,12 @@ class PokemonAssetView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    return Center(
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          if (backImage != null)
-            CachedNetworkImage(
-              imageUrl: backImage!,
-              cacheKey: backImage,
-              height: screenSize.height * 0.4,
-              fit: BoxFit.cover,
-            ),
-          Hero(
-            tag: frontImage,
-            child: CachedNetworkImage(
-              imageUrl: frontImage,
-              cacheKey: frontImage,
-              height: screenSize.height * 0.4,
-              // imageBuilder: (context, imageProvider) => Image(image: imageProvider),
-              // Container(
-              //   decoration: BoxDecoration(image: ),
-              // ),
-            ),
-          ),
-        ],
+    return Hero(
+      tag: frontImage,
+      child: CachedNetworkImage(
+        imageUrl: frontImage,
+        cacheKey: frontImage,
+        height: screenSize.height * 0.3,
       ),
     );
   }
