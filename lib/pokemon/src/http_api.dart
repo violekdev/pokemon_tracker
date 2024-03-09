084d16144e16b8e321a65ee182b32f51e362aeff
violekdev <71582866+violekdev@users.noreply.github.com>
2024-03-09 12:03:22 +0530

    Added Pokemon Data Caching

--- lib/pokemon/src/http_api.dart
+++ lib/pokemon/src/http_api.dart
@@ -1,9 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:pokemon_tracker/app/core/core.dart';
 import 'package:pokemon_tracker/pokemon/src/model.dart';
 
+// Global options
+final options = CacheOptions(
+  // A default store is required for interceptor.
+  store: MemCacheStore(),
+  // Returns a cached response on error but for statuses 401 & 403.
+  // Also allows to return a cached response on network errors (e.g. offline usage).
+  // Defaults to [null].
+  hitCacheOnErrorExcept: [401, 403],
+  // Overrides any HTTP directive to delete entry past this duration.
+  // Useful only when origin server has no cache config or custom behaviour is desired.
+  // Defaults to [null].
+  maxStale: const Duration(days: 7),
+);
+
 class PokemonAPI {
-  final _dio = Dio();
+  final _dio = Dio()..interceptors.add(DioCacheInterceptor(options: options));
 
   Future<Pokemon> fetchPokemonList(String url) async {
     try {
